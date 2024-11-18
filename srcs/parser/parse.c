d175559eeae2e8ca8bcb945f88266d54782b1b19
Eden Judah Amsalem <eamsalem@c1r4s5.42london.com>
2024-11-18 16:55:02 +0000

    added quote removal

--- srcs/parser/parse.c
+++ srcs/parser/parse.c
@@ -1,36 +1,58 @@
 #include "../../minishell.h"
 
-	// expand variables OUTSIDE of quotes, e.g. $USER, $HOME
-		// if " or ' encountered, skip until next " or '
-		// search string for '$'
-		// if text after $ is valid variable, expand variable
-		// else ... 
-	
-	// split input into "words", i.e. strings delimited by space, tab or \n
-	// text inside quotes is considered a single word
+void	remove_quotes(char *text)
+{
+	char	quote_to_del;
+	int		i;
 
-	// words are tokenized as COMMANDS, ARGUMENTS, KEYWORDS, SPECIAL SYMBOL
+	quote_to_del = *(ft_strchrset(text, QUOTES));
+	while (*text)
+	{
+		if (*text == quote_to_del)
+		{
+			i = 0;
+			while (text[i + 1])
+			{
+				text[i] = text[i + 1];
+				i++;
+			}
+			text[i] = '\0';
+		}
+		text++;
+	}
+}
 
-	// expand variables INSIDE double quotes but NOT single quotes
+void	quote_removal(t_list_2 *input)
+{
+	t_word	*word;
+
+	while (input)
+	{
+		word = (t_word *)(input->content);
+		if (ft_strchrset(word->text, QUOTES))
+			remove_quotes(word->text);
+		input = input->next;
+	}
+}
 
 t_list_2	*parse(char *input, t_dict *envp_dict)
 {
-	t_list_2	*parsed_input;
+	t_list_2	*split_input;
 	char		*expanded_input;
 	
 	expanded_input = expand_vars(input, envp_dict, IGNORE_QUOTED_VARS);
-	parsed_input = word_split(expanded_input);
-	expand_vars_inside_quotes(parsed_input, envp_dict);
-	//quote_removal();
+	split_input = word_split(expanded_input);
+	expand_vars_inside_quotes(split_input, envp_dict);
+	quote_removal(split_input);
 //	tokenise(parsed_input);
 	free(expanded_input);
-	return (parsed_input);
+	return (split_input);
 }
 
 
 int	main(int argc, char **argv, char **envp)
 {
-	char		*input = "\'$LANGUAGE\' \"$PATH\"";
+	char		*input = "\'\"\"$LANGUAGE\"\"\' hello\"$PATH\"";
 	t_list_2	*parsed_input;
 	t_list_2	*tmp;
 	t_dict		*envp_dict;
